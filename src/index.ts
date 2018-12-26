5ad86f5c044f72fb1993f692294f3ee8e75c2dbf
Sina Salehi <sinasalehi@Sinas-MacBook-Pro.local>
2018-12-25 22:52:08 -0500

    added .env

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
+import  * as dotenv from "dotenv";
 import { makeAugmentedSchema } from "neo4j-graphql-js";
 import { v1 as neo4j } from "neo4j-driver";
 import { ApolloServer } from "apollo-server";
 import typeDefs from "./typeDefs";
 
+dotenv.config();
 const driver = neo4j.driver(
-  "bolt://localhost:8888",
-  neo4j.auth.basic("neo4j", "neo4j")
+  process.env.NEO4J_BOLT_URL,
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASS)
 );
 
 const schema = makeAugmentedSchema({ typeDefs });
