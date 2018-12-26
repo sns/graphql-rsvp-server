31b5253381510e3a4d3a1b579267bb9ce9d44056
Sina Salehi <sinasalehi@Sinas-MacBook-Pro.local>
2018-12-26 01:24:05 -0500

    fixed dotenv.config is not a function

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import  * as dotenv from "dotenv";
+dotenv.config();
 import { makeAugmentedSchema } from "neo4j-graphql-js";
 import { v1 as neo4j } from "neo4j-driver";
 import { ApolloServer } from "apollo-server";
 import typeDefs from "./typeDefs";
 
-dotenv.config();
 const driver = neo4j.driver(
   process.env.NEO4J_BOLT_URL,
   neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASS)
